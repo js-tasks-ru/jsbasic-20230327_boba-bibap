eb3619987dd71082501e8f948e5c702a9be056e0
Nikita Kovalyov <boba.bibap@gmail.com>
2023-05-16 13:41:48 +0300

    6-module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,42 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
   constructor(rows) {
+    this.elem = render(rows)
   }
+}
+
+function makeHTML(arr) {
+  return `
+    <thead>
+          <tr>
+              <th>Имя</th>
+              <th>Возраст</th>
+              <th>Зарплата</th>
+              <th>Город</th>
+          </tr>
+      </thead>
+      <tbody>
+          ${arr.map(item => `
+      <tr>
+        <td>${item.name}</td>
+        <td>${item.age}</td>
+        <td>${item.salary}</td>
+        <td>${item.city}</td>
+        <td><button>X</button></td>
+      </tr>
+  `).join('')}
+      </tbody>`
+
+}
+
+
+function render(arr) {
+  const table = document.createElement("table");
+  table.innerHTML = makeHTML(arr);
+  const buttons = table.querySelectorAll("button")
+  console.log(buttons);
+  buttons.forEach(item => item.addEventListener('click', e =>
+    e.target.closest("tr").remove()))
+
+
+  return table;
 }
